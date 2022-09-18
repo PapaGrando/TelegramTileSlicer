2e4a44837ae29ee80e24ea88a072668feb3c34e7
PapaGrando <borislovesalo@gmail.com>
2022-09-19 02:44:55 +0300

    webhook refactor

--- TelegramCropper/Handlers/DocumentHandler.cs
+++ TelegramCropper/Handlers/DocumentHandler.cs
@@ -14,12 +14,12 @@ namespace TelegramCropper.Handlers
     public class DocumentHandler : BaseHandler
     {
         public override async Task Handle(ITelegramBotClient botClient, Update update, 
-            IChatRepo<IChatTask> chats, CancellationToken cancellationToken)
+            IChatRepo<IChatJob> chats, CancellationToken cancellationToken)
         {
             var chat = update.Message.Chat;
             var chatTask = chats.TryGetChat(chat.Id);
 
-            if (!await Utils.ChecksForDocument(botClient, chat.Id, chatTask, update.Message.Document.MimeType))
+            if (!await ChecksForDocument(botClient, chat.Id, chatTask, update.Message.Document.MimeType))
                 return;
 
             var fileInfo = await botClient.GetFileAsync(update.Message.Document.FileId);
@@ -46,11 +46,30 @@ namespace TelegramCropper.Handlers
             {
                 await botClient.SendTextMessageAsync(chat, ex.Message);
             }
-            //unknown ex
-            catch (Exception ex)
+        }
+
+        public async static Task<bool> ChecksForDocument(ITelegramBotClient bot,
+            long chatId, IChatJob? chatTask, string mime)
+        {
+            if (chatTask is null)
             {
-                Console.WriteLine($"\nImage Operation ERROR - {ex}");
+                await bot.SendTextMessageAsync(chatId, "First create config by /new before upload\n\n /help");
+                return false;
             }
+
+            if (chatTask.IsBusy)
+            {
+                await bot.SendTextMessageAsync(chatId, "Task processing... Wait");
+                return false;
+            }
+
+            if (mime != "image/png")
+            {
+                await bot.SendTextMessageAsync(chatId, "Currently only .PNG is supported. :(");
+                return false;
+            }
+
+            return true;
         }
     }
 }
